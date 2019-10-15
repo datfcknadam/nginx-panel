c9db36767b03d652d7796cc9a3e7dbba079c0cb5
Kulikov Egor <benstiller68@gmail.com>
2019-10-15 22:05:03 +0300

    small changes

--- src/components/Btn/AddEditBtn.vue
+++ src/components/Btn/AddEditBtn.vue
@@ -1,54 +1,70 @@
 <template>
   <v-dialog v-model="dialog" persistent max-width="600px">
     <template v-slot:activator="{ on }">
-      <v-btn :color="name === 'Добавить'? 'primary' : 'grey lighten-4'"
-      :dark="name === 'Добавить'? true : false" v-text="name" v-on="on"/>
+      <v-btn
+        :color="name === 'Добавить'? 'primary' : 'grey lighten-4'"
+        :dark="name === 'Добавить'? true : false"
+        v-text="name"
+        v-on="on"
+      />
     </template>
     <v-card>
       <v-card-title>
         <span class="headline title">{{name}} виртуальный хост с перенаправлением</span>
       </v-card-title>
-      <v-divider class="mx-2"/>
+      <v-divider class="mx-2" />
       <v-card-text>
         <v-container>
           <v-row>
             <v-col cols="12" sm="10" md="6">
-              <v-text-field v-model="host" outlined label="Виртуальный хост" required/>
+              <v-text-field v-model="host" outlined label="Виртуальный хост" required />
             </v-col>
             <v-col cols="12" sm="6" md="6">
-              <v-text-field  outlined label="Описание"/>
+              <v-text-field outlined label="Описание" />
             </v-col>
             <v-col cols="12">
-              <v-divider/>
+              <v-divider />
             </v-col>
-            <v-col cols="12" sm="10" md="10">
+            <v-col cols="12">
               <v-radio-group v-model="radioGroup">
-                <v-radio label="Перенаправлять HTTP на адрес"/>
-                <v-radio label="Перенаправлять HTTP на HTTPS"/>
+                <v-radio label="Перенаправлять HTTP на адрес" />
+                <v-radio label="Перенаправлять HTTP на HTTPS" />
               </v-radio-group>
-              <v-select :items="selectProp" v-model="selectHTTP" filled/>
-              <v-text-field label="Перенаправлять HTTP на адрес" outlined :value="selectHTTP" :disabled="radioGroup === 0 ? false : true"/>
+            </v-col>
+            <v-select :items="selectProp" v-model="selectHTTP" filled />
+            <v-text-field
+              label="Перенаправлять HTTP на адрес"
+              outlined
+              :value="selectHTTP"
+              :disabled="radioGroup === 0 ? false : true"
+            />
+            <v-col cols="12">
+              <v-divider />
+            </v-col>
+            <v-select :items="selectProp" v-model="selectHTTPS" filled />
+            <v-text-field
+              outlined
+              label="Перенаправлять HTTPS на адрес"
+              required
+              :value="selectHTTPS"
+            />
+            <v-col cols="12">
+              <v-divider />
             </v-col>
             <v-col cols="12">
-              <v-divider/>
-            </v-col>
-            <v-col cols="12" sm="10" md="10">
-              <v-select :items="selectProp" v-model="selectHTTPS" filled/>
-              <v-text-field outlined label="Перенаправлять HTTPS на адрес" required :value="selectHTTPS"/>
-            </v-col>
-            <v-col cols="12">
-              <v-divider/>
-            </v-col>
-            <v-col cols="12">
-              <v-checkbox label="Использовать сертификат LetsEnctrypt" :input-value="checkDomain ? false: null" :disabled="checkDomain ? true : false"/>
-              <v-checkbox label="Разрешать использование WebSocket"/>
+              <v-checkbox
+                label="Использовать сертификат LetsEnctrypt"
+                :input-value="checkDomain ? false: null"
+                :disabled="checkDomain ? true : false"
+              />
+              <v-checkbox label="Разрешать использование WebSocket" />
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="green lighten-1" dark  @click="dialog = false">Сохранить</v-btn>
+        <v-btn color="green lighten-1" dark @click="dialog = false">Сохранить</v-btn>
         <v-btn color="grey lighten-2" @click="dialog = false">Отмена</v-btn>
       </v-card-actions>
     </v-card>
@@ -56,30 +72,27 @@
 </template>
 <script>
 export default {
-  data(){
+  data() {
     return {
       host: "",
       description: "",
       radioGroup: 0,
       dialog: false,
-      selectProp: [
-        "http://",
-        "https://"
-      ],
+      selectProp: ["http://", "https://"],
       selectHTTP: "http://",
-      selectHTTPS: "http://",
-    }
+      selectHTTPS: "http://"
+    };
   },
   props: {
     name: {
-      type: String,
+      type: String
     },
     data: {
-      type: Number,
+      type: Number
     },
     identificator: {
-      type: Number,
-    },
+      type: Number
+    }
   },
   computed: {
     checkDomain() {
@@ -87,14 +100,18 @@ export default {
         return true;
       }
       return false;
-    },
-  },
-}
+    }
+  }
+};
 </script>
 <style>
 div.v-input.v-select {
   max-width: 120px;
   padding: 0;
   margin: 0;
+}
+.v-text-field--filled > .v-input__control > .v-input__slot {
+  border-top-left-radius: 16px !important;
+  border-top-right-radius: 0px !important;
 }
 </style>
