72f091ba00fc9e805a785abeed18d2e1b95137ae
Kulikov Egor <benstiller68@gmail.com>
2019-10-15 18:26:49 +0300

    added material-icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
+import 'material-icons/iconfont/material-icons.scss';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify);
 
