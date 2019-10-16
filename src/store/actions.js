64c07781ca07ff9fdd52fe2c40f0f61a15338749
Kulikov Egor <benstiller68@gmail.com>
2019-10-17 00:14:47 +0300

    small changes

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 export default {
   loadHostList({commit}){
     axios
-      .get('http://localhost:3030/')
+      .get('http://localhost:8888/')
       .then((response) => {
-        commit("SET_HOST_LIST", response);
+        commit("SET_HOST_LIST", response.data);
       // eslint-disable-next-line no-console
       }).catch(error => console.log(error.response));
   },
